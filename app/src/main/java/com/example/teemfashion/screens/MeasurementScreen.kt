481539be602a7e26bf4ae05cbe3e590a68d5f4bd
haywhy2019 <olusegun.sola19@gmail.com>
2024-10-21 13:05:49 +0100

    added folders

---
+++ app/src/main/java/com/example/teemfashion/screens/MeasurementScreen.kt
@@ -0,0 +1,6 @@
+package com.example.teemfashion.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MeasurementScreen(){}
---
+++ app/src/main/java/com/example/teemfashion/screens/RegisterScreen.kt
@@ -0,0 +1,9 @@
+package com.example.teemfashion.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RegisterScreen(){
+    Text(text = "Register")
+}
